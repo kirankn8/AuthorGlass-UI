30473a77a3fb9a427728baee31961a9daa16e6c3
kirankn8 <kirankn8@gmail.com>
2017-11-16 00:14:45 +0530

    Adding navbar and  suggest component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SuggestComponent } from './searchandsuggest/suggest.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SuggestComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/searchandsuggest/suggest.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'suggest',
+  templateUrl: './suggest.component.html',
+  styleUrls: ['./suggest.component.css']
+})
+export class SuggestComponent {
+  
+}
