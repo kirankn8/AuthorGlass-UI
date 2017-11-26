180d499561def4b1b7f9f98693b9815a54eb6764
kirankn8 <kirankn8@gmail.com>
2017-11-26 15:56:26 +0530

    Adding flipping animation between pages

---
+++ src/app/story/display-story/read/content.ts
@@ -0,0 +1,9 @@
+export class Content {
+    pageNo: number;
+    content: string;
+
+    constructor(pageNo: number, content: string){
+        this.pageNo = pageNo;
+        this.content = content;
+    }
+}
