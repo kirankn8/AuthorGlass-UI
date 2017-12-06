5ccddb602e9c342c6dcc2cf45ceab3d8d698766a
kirankn8 <kirankn8@gmail.com>
2017-12-06 23:20:14 +0530

    Fixing the close on save in Creative sdk

--- src/app/story/shared/creative-sdk/creative-sdk.component.ts
+++ src/app/story/shared/creative-sdk/creative-sdk.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 
 declare var Aviary: any;
-
+var self;
 @Component({
   selector: 'creative-sdk',
   templateUrl: './creative-sdk.component.html',
@@ -16,21 +16,33 @@ export class CreativeSdkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let self = this;
     this.featherEditor = new Aviary.Feather({
       apiKey: '3bfcd2b9623d45b98301fb7d037425a2',
       // tools : 'enhance,text,meme,draw,crop,resize,effects,frames,sharpness,whiten',
       onSave: function(imageID, newURL) {
-          var img = document.getElementById(imageID) as HTMLImageElement;
-          img.src = newURL;
+        var img = document.getElementById(imageID) as HTMLImageElement;
+        img.src = newURL;
+        self.featherEditor.close();
+      },      
+      onClose(isDirty) {
+        self.featherEditor.close();
       }
     });
   }
 
+
+  closeEditor(){
+    console.log(this.featherEditor)
+  }
+
   launchEditor(id, src) {
+    console.log(this.featherEditor);
     this.featherEditor.launch({
         image: id,
         url: src,
-        // forceCropPreset: ['Coverpage','150x150'],
+        forceCropPreset: ['Coverpage','210x297'],
+        forceCropMessage: 'Crop your profile picture:',
     });
     return false;
   }
