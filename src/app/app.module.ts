3d102aad181d613c5d9eec0be7a9f93ee62ab2e5
kirankn8 <kirankn8@gmail.com>
2017-12-07 20:46:51 +0530

    Restructuring the project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SuggestComponent } from './suggest/suggest.component';
-import { FooterComponent } from './footer/footer.component'
+import { NavbarComponent } from './common_components/navbar/navbar.component';
+import { SuggestComponent } from './common_components/suggest/suggest.component';
+import { FooterComponent } from './common_components/footer/footer.component'
 
 /* Routing Module */
 import { AppRoutingModule }   from './app-routing.module';
@@ -14,6 +14,7 @@ import { AuthenticationModule } from './authentication/authentication.module';
 import { HomeModule } from './home/home.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { SearchComponent } from './search/search.component';
+import { CardBookComponent } from './shared_components/card-book/card-book.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { SearchComponent } from './search/search.component';
     SuggestComponent,
     FooterComponent,
     SearchComponent,
+    CardBookComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared_components/card-book/card-book.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-card-book',
+  templateUrl: './card-book.component.html',
+  styleUrls: ['./card-book.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CardBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
