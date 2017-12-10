534733664c8c32b56efff4a4468e61f5d3d736e5
kirankn8 <kirankn8@gmail.com>
2017-12-10 15:59:07 +0530

    Designing home page, card layout, favicons

--- src/app/home/recommendations/recommendations.component.ts
+++ src/app/home/recommendations/recommendations.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class RecommendationsComponent implements OnInit {
 
+  books = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
   constructor() { }
 
   ngOnInit() {
