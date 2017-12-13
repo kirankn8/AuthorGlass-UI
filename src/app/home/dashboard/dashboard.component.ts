757920f5030889d8175bd077c2eab2329c1b52b5
kirankn8 <kirankn8@gmail.com>
2017-12-13 20:28:01 +0530

    Adding dashboard milestone points

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -29,27 +29,4 @@ export class DashboardComponent implements OnInit {
   public pieChartLegend:boolean = true;
   public pieChartType:string = 'doughnut';
  
-
-  // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-  public lineChartColors:Array<any> = [ ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-
-
-
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
- 
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
 }
